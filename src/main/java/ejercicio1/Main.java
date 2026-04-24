1a2587ab90f6c690921db7e44522303bf2c09189
PedroContrera1 <pedrocontrera1745@gmail.com>
2026-04-24 17:06:10 -0300

    MAL

---
+++ src/main/java/ejercicio1/Main.java
@@ -0,0 +1,16 @@
+package ejercicio1;
+import java.awt.EventQueue;
+import java.sql.SQLException;
+public class Main {
+    public static void main(String[] args) throws SQLException {
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                try {
+                    new AgregarParticipante();
+                } catch (Exception e) {
+                    System.out.println(e);
+                }
+            }
+        });
+    }
+}
